a417370ca040061170d6077c5b054cbf1b402b8f
ashutoshsaini408 <ashutosh.saini@burning-desire.in>
2021-09-25 21:31:28 +0530

    Header and Hero section

--- src/web/components/Layout/Navigation/index.js
+++ src/web/components/Layout/Navigation/index.js
@@ -1,31 +1,53 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {   } from 'react-router-dom'
+import {Nav,Button} from 'react-bootstrap'
+
 import TopMenu from '../TopMenu'
 import './index.css'
 
 export default function Navigation() {
       return (
             <>
-            <TopMenu />
-            <nav className="main-navigation">
-            
-            <ul>
-                   <li>
-                    <Link to="/">Home</Link>
-                </li>
+            <header id="header">
                 
-            </ul>
-            <img src="/logo.png" className="logo-menu"/>  
-            <ul>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/register">Register</Link>
-                </li>
-            </ul>
-
-        </nav>
+                {/* <TopMenu /> */}
+                <nav classNameName="main-navigation">
+                    <div className="header-main">
+                        <div className="row">
+                            <div className="col-lg-3 col-md-12 col-12">
+                                <div className="web-logo">
+                                    <img src="/logo.png" className="img-fluid"/> 
+                                </div>
+                                
+                            </div>
+                            <div className="col-lg-6 col-md-12 col-12 align-item-center jusify-content-center">
+                                <Nav activeKey="/home" >
+                                    <Nav.Item>
+                                        <Nav.Link className="head-item" href="/">Home</Nav.Link>
+                                    </Nav.Item>
+                                    <Nav.Item>
+                                        <Nav.Link className="head-item" href="/">About</Nav.Link>
+                                    </Nav.Item>
+                                    <Nav.Item>
+                                        <Nav.Link className="head-item" href="/">Test</Nav.Link>
+                                    </Nav.Item>
+                                    <Nav.Item>
+                                        <Nav.Link className="head-item" href="/">Test</Nav.Link>
+                                    </Nav.Item>
+                                    
+                                </Nav>
+                            </div>
+                            <div className="col-lg-3 col-md-12 col-12">
+                                <div className="header-side-btn">
+                                    <Button className="web-btn" href="/login" >Login</Button> {' '}
+                                    <Button className="web-btn" href="/register" >Register</Button> {' '}
+                                </div>  
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+         
+            </header>
         </>
       )
 }
--- src/web/container/Home/index.js
+++ src/web/container/Home/index.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import Navigation from '../../components/Layout/Navigation'
+import {Button} from 'react-bootstrap'
 import './index.css';
 
 export default function Home() {
       return (
             <div>
-                  <Navigation />
-                  <h2>Welcome to Home Page</h2>
+                  <div className="container">
+                        <Navigation />
+                        <section className="hero-section sec-padding">
+                              <div className="row">
+                                    <div className="col-lg-6 col-md-6 col-12">
+                                          <div class="hero-content">
+                                                <div class="hero-title">
+                                                      <h1 className="hero-title-dark">Investing in</h1>
+                                                      <h1 className="hero-title-dark">Knowledge and </h1>
+                                                      <h1 className="hero-title-red">Your future</h1>
+                                                </div>
+                                                <div className="hero-sub-title">
+                                                      <p>With the help of own learning create your own path and</p>
+                                                      <p>drive your own skills on your own to archive what you seek</p>
+                                                </div>
+                                                <div className="hero-btn">
+                                                      <Button className="web-btn" href="/register" >Enroll Now</Button> {' '}
+                                                </div>
+                                          </div>
+                                    </div>
+                                    <div className="col-lg-6 col-md-6 col-12">
+                                          <div className="hero-image">
+                                                <img src="/hero1.jpg" className="img-fluid"/> 
+                                          </div>
+                                    </div>
+                              </div>
+                        </section>
+                  </div>
             </div>
       )
 }
