9ea98808562762b7920f196d1c51de5371c4a9fc
rohit singh <rohit.singh@firstprinciples.io>
2021-10-07 23:42:41 +0530

    why learn javascript add images and section

--- src/web/components/Home/WhyLearnJavascript/index.js
+++ src/web/components/Home/WhyLearnJavascript/index.js
@@ -1,28 +1,99 @@
-import React from 'react'
+import React from "react";
 function WhyLearnJavascript() {
-      return (
-            <section className="hero-section sec-padding">
-                  <div className="row">
-                        <div className="col-md-10 offset-md-1">
-                        <h2 className="pb-4">Why Learn Javascript ?</h2>
-                        <p>
-                        You may be asking what programming languages to learn if you want to work as a web developer. Not only that, but you'll want to know which languages to prioritise. Do you begin with the simplest? Or the ones that are the most useful? Both? Why study JavaScript these days, when there are so many options?
-                        </p>
-                        <p>
-                        You've probably heard that JavaScript is an important online technology, but you're not sure if it's still relevant today. Is it really necessary to learn JavaScript from scratch when there are so many frameworks that provide ready-to-use code?
-                        </p>
-                        <p>
-                        We'll look at what JavaScript does and if it's still helpful in this guide. After that, we'll go through some of the most compelling reasons to learn JavaScript, as well as why it's important to learn plain JavaScript first, before moving on to libraries and frameworks.
-                        </p>
+  return (
+    <section className="hero-section sec-padding">
+      <div className="row">
+        <div className="col-md-3">
+            <h2 className="pb-4">Why Learn Javascript ?</h2>
+            <img src="/javascript.png" alt="javascript most popular language" style={{ width: '80%'}}/>
+            <br />
+            <img src="/nodejs.jpg" alt="Nodejs Most popular backend runtime enviornment" style={{ width: '80%', margin: '10vh 0px'}}/>
+            <br />
+            <img src="/electron.jpg" alt="Nodejs Most popular backend runtime enviornment" style={{ width: '80%', margin: '10vh 0px'}}/>
+        </div>    
+        <div className="col-md-9">
+          <p>
+            You may be asking what programming languages to learn if you want to
+            work as a web developer. Not only that, but you'll want to know
+            which languages to prioritise. Do you begin with the simplest? Or
+            the ones that are the most useful? Both? Why study JavaScript these
+            days, when there are so many options?
+          </p>
+          <p>
+            You've probably heard that JavaScript is an important online
+            technology, but you're not sure if it's still relevant today. Is it
+            really necessary to learn JavaScript from scratch when there are so
+            many frameworks that provide ready-to-use code?
+          </p>
+          <p>
+            We'll look at what JavaScript does and if it's still helpful in this
+            guide. After that, we'll go through some of the most compelling
+            reasons to learn JavaScript, as well as why it's important to learn
+            plain JavaScript first, before moving on to libraries and
+            frameworks.
+          </p>
 
-                        <h3>What is JavaScript and what does it do?</h3>
-                        <p>It's critical to know exactly what something is and what it does before you begin learning it. This is very helpful when learning a new programming language.</p>
-                        <p>JavaScript is a programming language that is used to create interactive websites. If you consider the core components of a website, you have HTML, which explains and specifies the website's basic content and structure, and CSS, which informs the browser how to display this HTML information, defining things like colour and font.</p>
-                        </div>
-                        
-                  </div>
-            </section>
-      )
+          <h3>What is JavaScript and what does it do?</h3>
+          <p>
+            It's critical to know exactly what something is and what it does
+            before you begin learning it. This is very helpful when learning a
+            new programming language.
+          </p>
+          <p>
+            JavaScript is a programming language that is used to create
+            interactive websites. If you consider the core components of a
+            website, you have HTML, which explains and specifies the website's
+            basic content and structure, and CSS, which informs the browser how
+            to display this HTML information, defining things like colour and
+            font.
+          </p>
+
+          <br />
+          <br />
+          <h3>JavaScript experts are versatile</h3>
+          <p>
+            JavaScript is a very flexible programming language. The
+            possibilities are unlimited once you've learned it: you can use
+            React Js to code on the client-side (frontend) and Node.js to code
+            on the server-side (backend). You can also use React, React Native,
+            and Electron to create web, mobile, and desktop apps, as well as get
+            into machine learning.
+          </p>
+          <p>
+            JavaScript is required if you wish to work as a frontend developer.
+            As a JS specialist, though, it isn't the only job option available
+            to you. You might work in full-stack development, games development,
+            information security software engineering, machine learning, and
+            artificial intelligence after mastering this important programming
+            language, to name a few!
+          </p>
+          <p>
+            Finally, if you want to work in development or engineering, you'll
+            need to know JavaScript.
+          </p>
+
+          <br />
+          <br />
+          <h3>JavaScript experts are in-demand (and well-paid)</h3>
+          <p>
+            Because JavaScript is the most widely used programming language on
+            the planet, it's no surprise that it's one of the most in-demand web
+            development abilities today.
+          </p>
+          <p>
+            Simultaneously, the global demand for the language appears to
+            outnumber the available competence. According to a HackerRank
+            research, 48 percent of firms worldwide want JavaScript engineers,
+            while just 42% of student developers claim to be fluent in the
+            language. And, according to HackerRank's most recent report for
+            2020, the most popular language that hiring managers look for in a
+            web development candidate is JavaScript.
+          </p>
+
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default WhyLearnJavascript
+export default WhyLearnJavascript;
